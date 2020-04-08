88e6684fa94c3bb3251e106e4aaae06be5b9aa7b
Charles Sieg <charles.sieg@renkara.com>
2020-04-08 02:29:13 -0500

    Initial commit.

---
+++ main.tf
@@ -0,0 +1,101 @@
+provider "aws" {
+  alias = "dnsProvider"
+}
+
+provider "aws" {
+  alias = "tools"
+}
+
+###################################################################
+# S3 BUCKET
+###################################################################
+
+resource "aws_s3_bucket" "bucket" {
+  acl           = "private"
+  bucket        = var.bucket_name
+  force_destroy = true
+
+  tags = {
+    Name        = var.bucket_name
+    Billing     = var.environment
+    Environment = var.environment
+    Terraform   = "true"
+  }
+
+  website {
+    index_document = "index.html"
+    error_document = "404.html"
+  }
+}
+
+###################################################################
+# CLOUDFRONT
+###################################################################
+
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "CloudFront Identity for ${var.app_name}"
+}
+
+module "cloudfront" {
+  app_name               = var.app_name
+  aws_region             = var.aws_region
+  bucket_name            = aws_s3_bucket.bucket.id
+  cloudfront_ttl         = var.cloudfront_ttl
+  environment            = var.environment
+  origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+  source                 = "../cloudfront"
+}
+
+data "aws_iam_policy_document" "policy_document" {
+  statement {
+    actions   = ["s3:*"]
+    resources = ["${aws_s3_bucket.bucket.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${local.account_id}:root"]
+    }
+  }
+
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.bucket.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn]
+    }
+  }
+
+  statement {
+    actions   = ["s3:ListBucket"]
+    resources = [aws_s3_bucket.bucket.arn]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn]
+    }
+  }
+}
+
+// Create a bucket policy allowing the tools account to put objects into the bucket.
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = data.aws_iam_policy_document.policy_document.json
+}
+
+module "website_charlessieg" {
+  app_name        = "website-charlessieg"
+  aws_region      = data.aws_region.current.name
+  bucket_name     = "${var.subdomain}.charlessieg.com"
+  cloudfront_ttl  = var.cloudfront_ttl
+  create_secrets  = true
+  domain_zone_id  = data.aws_route53_zone.charlessieg.zone_id
+  environment     = var.environment
+  github_repo_url = "https://github.com/CharlesSieg/website-charlessieg-source.git"
+  providers = {
+    aws.dnsProvider = aws.dnsProvider
+    aws.tools       = aws.tools
+  }
+  source = "https://github.com/CharlesSieg/terraform-module-aws-static-website.git"
+}
---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "app_name" {
+  description = "The app name used for tagging infrastructure."
+  type        = string
+}
+
+variable "aws_region" {
+  description = "The AWS region in which the infrastructure will be provisioned."
+  type        = string
+}
+
+variable "bucket_name" {
+  description = ""
+  type        = string
+}
+
+variable "cloudfront_ttl" {
+  description = "How many seconds an object remains in cache."
+  type        = number
+}
+
+variable "domain_zone_id" {
+  description = "DNS zone where host names will be created."
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment in which this infrastructure will be provisioned."
+  type        = string
+}
+
+variable "github_repo_url" {
+  description = ""
+  type        = string
+}
